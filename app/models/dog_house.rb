5180a2ba66207f9b8cf0002deb3dc545e75b8ecc
Ndiema-S <you@example.com>
2022-12-01 12:21:09 +0300

    complete assignment

--- app/models/dog_house.rb
+++ app/models/dog_house.rb
@@ -1,3 +1,15 @@
-class DogHouse < ApplicationRecord
-  has_many :reviews
+class DogHousesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def show
+    dog_house = DogHouse.find(params[:id])
+    render json: dog_house
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Dog house not found" }, status: :not_found
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
-class Review < ApplicationRecord
-  belongs_to :dog_house
+def index
+  if params[:dog_house_id]
+    dog_house = DogHouse.find(params[:dog_house_id])
+    reviews = dog_house.reviews
+  else
+    reviews = Review.all
+  end
+  render json: reviews, include: :dog_house
 end
